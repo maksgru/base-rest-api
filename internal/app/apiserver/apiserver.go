e826c4b1c59e0ca515021fbc6548e81a78822ea3
Maks <maksgru@hotmal.com>
2022-07-08 22:00:27 +0300

    Add user repository

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/maksgru/base-rest-api/internal/app/store"
 	"github.com/sirupsen/logrus"
 )
 
@@ -12,6 +13,7 @@ type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
 
 func New(config *Config) *APIServer {
@@ -28,6 +30,10 @@ func (s *APIServer) Start() error {
 	}
 
 	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
 
 	s.logger.Info("Starting API server")
 
@@ -46,6 +52,18 @@ func (s *APIServer) configureLogger() error {
 
 func (s *APIServer) configureRouter() {
 	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+
+	s.store = st
+
+	if err := s.store.Open(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *APIServer) handleHello() http.HandlerFunc {
